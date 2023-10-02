a7e4466a5c17bb2fdfb740f21ca26711075f3253
Muhammad Ishaq <97754054+Ishaq120@users.noreply.github.com>
2023-10-02 04:16:22 +0000

    These are lecture 3 and 4

---
+++ 03_variable.js
@@ -0,0 +1,8 @@
+let userName = "M Ishaq";
+const password = "helloishaq";
+var userId = "ishaq123"; // This method of declearation is nor prefered only use let and const
+userCity = "Sargodha"; // These are possibilities that variables are decleared in this way but nor prefered
+
+console.log(userName);
+// to print whole data in table formate at once 
+console.table([userName, userId, password, userCity]);
---
+++ 04_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // => This line tell JS Engine that if some code of js is written in old JS formate then treat it as a new version of JS syntax.
+
+// Datatypes in JS 
+/* 
+========== Permitive data Types ==============
+1) String =  " "
+2) Number = 1,2 3 .... upto 2 to powr 53
+3) Boolean = true/false
+4) undefiend = variable decleared but not have any value
+5) null  = stand alone value [this a object data type]
+6) Symbole = unique
+
+============== Non Premitive data types ===========
+array
+object  null is a object data type
+*/
