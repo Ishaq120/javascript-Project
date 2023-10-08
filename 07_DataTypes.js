1100622ef9f32bfdba86fac5234756e1a00b6308
Muhammad Ishaq <97754054+Ishaq120@users.noreply.github.com>
2023-10-08 17:48:05 +0000

    Data types and return of datatypes

---
+++ 07_DataTypes.js
@@ -0,0 +1,11 @@
+// =======^^^^^^ There are two datatypes in js ^^^^^========
+// 1) Perimitive (7)= String, Number, Boolean,undefined, null, symbols, BigInt
+// 2) Non-Premitive (Refrence value) (3)= Array, Objects, Function
+
+// In Premitive datatypes if you call them or copy them, you will get a copy of that data you will not get the refrence of this datatype. If you change there values at any place then changes will be happen in copy. Origional values remain same
+
+// In Non-Premitive datatype you will get the refrence of memory 
+
+// ============^^^^^^^^ Interview Question ^^^^^^^==============
+// Q) Is JS a dynamic or static programming Language
+// Ans) Javascript is a dynamic typing language. When you declare a variable, you do not need to specify what type this variable is. Javascript engine infers what type this variable is based on the value assigned to at run time.
