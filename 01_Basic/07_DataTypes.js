1a7cb76aea9273538f90a6618108de13a8c8dbc6
Muhammad Ishaq <97754054+Ishaq120@users.noreply.github.com>
2023-10-12 04:24:42 +0000

    Js by Hetish Chaudry

--- 01_Basic/07_DataTypes.js
+++ 01_Basic/07_DataTypes.js
@@ -5,6 +5,7 @@
 // In Premitive datatypes if you call them or copy them, you will get a copy of that data you will not get the refrence of this datatype. If you change there values at any place then changes will be happen in copy. Origional values remain same
 
 // In Non-Premitive datatype you will get the refrence of memory 
+// If you write "n" in front of a big number like => "587456471589416975n" then this called "BigInt"
 
 // ============^^^^^^^^ Interview Question ^^^^^^^==============
 // Q) Is JS a dynamic or static programming Language
